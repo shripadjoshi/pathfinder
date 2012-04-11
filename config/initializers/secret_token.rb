3c052b49ff3e86c96c492d6eaab23feaf2cd35eb
shripad <shripad@shripad.(none)>
2012-04-11 09:30:46 +0530

    Application commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pathfinder::Application.config.secret_token = 'bb3295023c6c575b300fc4bdbccb49f00909eedeeb0ba7b9833f0ee7594a2a2b2496f99b67e2669133debd09766facac06a926f628fe803099e7aef19540ff94'
